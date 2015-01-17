39526a20d527dcf67fbaff006e1350704792159a
Ray Farias <ray@rayfarias.com>
2015-01-16 21:10:14 -1000

    fix (gulpfile.js): update paths to point to proper directories

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ var gutil = require('gulp-util');
 var watch = require('gulp-watch');
 
 gulp.task('mocha', function () {
-  return gulp.src(['test/*.spec.js'], { read: false })
+  return gulp.src(['spec/*.js'], { read: false })
             .pipe(mocha({ reporter: 'spec' }))
             .on('error', gutil.log);
 });
 
 gulp.task('watch-mocha', function () {
-  gulp.watch(['./**.js', 'test/*.spec.js'], ['mocha']);
+  gulp.watch(['./**.js', 'spec/*.js'], ['mocha']);
 });
 
 gulp.task('test', ['mocha', 'watch-mocha']);
