513a17da7772180bac693832a0dbfbd47d1d83dd
Ray Farias <ray@rayfarias.com>
2015-01-20 16:14:09 -1000

    update (project): ready for Andrew to do work with

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # 100 Arrays and Objects
-An exercise for creating Arrays and Objects then creating functions to access and manipulate the data stored inside.
+An exercise for accessing data inside of Array, Objects, and everything in-between.
 
 # Getting Started
 - Install dependencies: `npm install`
 
 - Do your work in the file: `main.js`
-
--- spec/100-array-objects.js
+++ spec/100-array-objects.js
@@ -14,12 +14,22 @@ var _ = require('lib/test-helpers');
 
 
 describe('Accessing Data inside of the answers object', function() {
- describe('Hawaiian Islands', function () {
-  it('each major island should exist as a property', function () {
+ describe('Hawaiian Island Property names', function () {
+  it('should have each major island should exist as a property', function () {
     keys.islands.forEach(function(curr, val) {
       expect(answers).to.have.property(curr);
     });
   });
+  it('should have each value should have the island name as a String', function () {
+    keys.islands.forEach(function(curr, val) {
+      expect(answers[curr]).to.be.a('string');
+      expect(answers[curr].toLowerCase()).to.equal(curr);
+    });
+  });
+ }); // desc('Hawaiian Island')
+ describe('User Data', function () {
+  
+   
  });
 
-}); // end desc('Create Arrays')
+}); // end desc('Accessing Data...')
